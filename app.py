c723586f97af129605715c0715488e1b1214c9f4
himanshushukla254 <himanshushukla254@gmail.com>
2017-05-24 23:42:06 +0530

    init

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-	return 'Yo, its working!'
-if __name__ == "__main__":
-	app.run()
+from flask import Flask,url_for,render_template, flash, request, redirect
+from wtforms import Form
+from gensim.models import word2vec
+import gc
+import os
+
+
+app=Flask(__name__)
+
+@app.route('/') 
+def hello():
+    return render_template('home.html');
+
+
+
+@app.route('/output/' , methods=['GET','POST'])
+def output():
+    input1 = str(request.form['str'])   
+    model = word2vec.Word2Vec.load_word2vec_format('static/text.model.bin', binary=True)
+    res = model.most_similar(input1,topn=5)
+    return render_template('output.html', input1=res)	
+    
+	
+	
+if __name__=='__main__':
+    app.run()
