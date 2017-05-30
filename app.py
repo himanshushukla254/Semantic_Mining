316e48a1fbd3f06fb424d4551f622c4c47aaa521
himanshushukla254 <himanshushukla254@gmail.com>
2017-05-30 19:19:39 +0530

    second commit

--- app.py
+++ app.py
@@ -15,11 +15,29 @@ def hello():
 
 @app.route('/output/' , methods=['GET','POST'])
 def output():
-    input1 = str(request.form['str'])   
+    input1 = str(request.form['str'])
     model = word2vec.Word2Vec.load_word2vec_format('static/text.model.bin', binary=True)
     res = model.most_similar(input1,topn=5)
     return render_template('output.html', input1=res)	
-    
+
+@app.route('/output1/' , methods=['GET','POST'])
+def output1():
+    input2 = str(request.form['str2']);  
+    input3 = str(request.form['str3']); 
+    input4 = str(request.form['str4']);  
+    model = word2vec.Word2Vec.load_word2vec_format('static/text.model.bin', binary=True)
+    res1 = model.most_similar((input2,input3),input4,topn=5);
+    return render_template('output1.html', input1=res1)
+
+@app.route('/output2/' , methods=['GET','POST'])
+def output2():
+    input5 = str(request.form['str5']);  
+    input6 = str(request.form['str6']);  
+    input7 = str(request.form['str7']);
+    input8 = str(request.form['str8']); 
+    model = word2vec.Word2Vec.load_word2vec_format('static/text.model.bin', binary=True)
+    res2 = model.doesnt_match((input5,input6,input7,input8));
+    return render_template('output2.html', input1=res2)	
 	
 	
 if __name__=='__main__':
